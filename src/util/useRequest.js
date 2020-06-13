08b7a6b85b3d2bfae169c5f239ae3e151c9adb76
Matias Tucci <matiastucci@gmail.com>
2020-06-14 01:00:21 +0200

    update useRequest

--- src/util/useRequest.js
+++ src/util/useRequest.js
@@ -1,18 +1,10 @@
 import useSWR from 'swr'
 import axios from 'axios'
 
-export default function useRequest(request, { initialData, ...config } = {}) {
+export default function useRequest(request, config = {}) {
   return useSWR(
     request && JSON.stringify(request),
     () => axios(request || {}).then(response => response.data),
-    {
-      ...config,
-      initialData: initialData && {
-        status: 200,
-        statusText: 'InitialData',
-        headers: {},
-        data: initialData
-      }
-    }
+    config
   )
 }
