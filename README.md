6be1afcb07637caefe601727b350ededcd373e92
gillchristian <gillchristiang@gmail.com>
2020-06-18 14:23:43 +0200

    fix: tresh -> thresh

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Tresh Dashboard
+# Thresh Dashboard
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
     <div className="center">
-      <h1>TRESH</h1>
+      <h1>THRESH</h1>
     </div>
   )
 }
